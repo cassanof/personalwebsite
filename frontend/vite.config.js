fe3ca4b61a89a4dbd577a5bd9f5741247c5bd51a
elleven11 <federico.cassano@federico.codes>
2023-05-29 21:54:19 -0700

    handlebars publications

---
+++ frontend/vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+export default {
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'partials'),
+    }),
+  ],
+};
+
