d05db5b4734ba73724dbc753d5b02b817456cbc6
elleven11 <federico.cassano@federico.codes>
2023-08-21 00:52:37 -0700

    chessing...

--- frontend/js/sketch.js
+++ frontend/js/sketch.js
@@ -5,16 +5,45 @@ function resizeBgIfNeeded() {
   if (currWidth != window.innerWidth || currHeight != window.innerHeight) {
     currWidth = window.innerWidth;
     currHeight = window.innerHeight;
-    resizeCanvas(currWidth, currHeight);
+    resizeCanvas(currWidth / 5, currWidth / 5);
+    noStroke();
+    background(20);
   }
 }
 
 function setup() {
   // full screen canvas
-  let canvas = createCanvas(currWidth, currHeight);
-  canvas.parent("body");
+  let canvas = createCanvas(currWidth / 5, currWidth / 5);
+  canvas.parent("chess");
+  // make it relative to the parent
+  canvas.style("position", "relative");
+  noStroke();
+  background(20);
+}
+
+function drawBoard() {
+  const size = 5;
+  for (let i1 = 0, d = 1; i1 < size; i1++) {
+    for (let i2 = 0; i2 <= 10; i2++) {
+      if (i2 % 2 != 0) {
+        fill(255);
+        rect(
+          i1 * (height / size),
+          i2 * (height / size) - d * (height / size),
+          height / size,
+          height / size
+        );
+      }
+    }
+    if (i1 % 2 != 0) {
+      d = 1;
+    } else {
+      d = 0;
+    }
+  }
 }
 
 function draw() {
   resizeBgIfNeeded();
+  drawBoard();
 }
