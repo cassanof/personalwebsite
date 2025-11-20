2bda9c795863c95bb8edf8794d34ddb430f9bcfd
cassanof <federico.cassano@federico.codes>
2025-11-19 23:10:43 -0800

    clean up main.go

--- main.go
+++ main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"io"
 	"log"
-	"net/http"
 	"os"
-	"strconv"
-	"strings"
-	"sync"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
-
-type GenerateRequest struct {
-	Prompt string `json:"prompt"`
-}
-
-type GenerateResponse struct {
-	Generated string `json:"generated"`
-}
-
-type GenerateRequestQueued struct {
-	req           GenerateRequest
-	responseQueue chan *GenerateResponse
-}
-
-type Cache struct {
-	Requests map[string]*GenerateResponse
-	Lock     sync.RWMutex
-}
 
 const (
 	STATIC_PATH = "./frontend/dist/"
@@ -46,55 +23,9 @@ var (
 		GETOnly:       false,
 		AppName:       "Federico's Personal Website",
 	})
-	MAX_QUEUE_SIZE    = 1
-	API_KEY           = ""
-	ENGINE            = ""
-	MODEL_ENDPOINT    = "https://api-inference.huggingface.co/models/gammatau/deepseek-1b-multipl-t-rkt"
-	LLAMACPP_ENDPOINT = "http://localhost:8000/completion"
-	generateQueue     = make(chan GenerateRequestQueued, MAX_QUEUE_SIZE)
-	cache             = Cache{
-		Requests: make(map[string]*GenerateResponse),
-		Lock:     sync.RWMutex{},
-	}
 )
 
 func main() {
-	// get engine for completion
-	ENGINE = os.Getenv("ENGINE")
-	switch ENGINE {
-	case "HF":
-		ENGINE = "HF"
-	case "LLAMACPP":
-		ENGINE = "LLAMACPP"
-		if os.Getenv("LLAMACPP_ENDPOINT") != "" {
-			LLAMACPP_ENDPOINT = os.Getenv("LLAMACPP_ENDPOINT")
-		}
-	default:
-		// default to HF
-		ENGINE = "HF"
-	}
-
-	if ENGINE == "HF" {
-		API_KEY = os.Getenv("API_KEY")
-		if API_KEY == "" {
-			log.Fatal("huggingface API key not set!! set it with the API_KEY environment variable. Or use llamacpp as the engine with ENGINE=LLAMACPP")
-		}
-		if os.Getenv("MODEL_ENDPOINT") != "" {
-			MODEL_ENDPOINT = os.Getenv("MODEL_ENDPOINT")
-		}
-	}
-
-	// set max queue size to env variable if set
-	// otherwise, use default value
-	if os.Getenv("MAX_QUEUE_SIZE") != "" {
-		str_max_queue_size := os.Getenv("MAX_QUEUE_SIZE")
-		i, err := strconv.Atoi(str_max_queue_size)
-		if err != nil {
-			log.Fatal("MAX_QUEUE_SIZE must be an integer")
-		}
-		MAX_QUEUE_SIZE = i
-	}
-
 	file := createLogFile()
 	defer file.Close()
 
@@ -109,11 +40,6 @@ func main() {
 		Level: compress.LevelBestCompression,
 	}))
 
-	app.Post("/generate", generateEndpoint)
-	for i := 0; i < MAX_QUEUE_SIZE; i++ {
-		go generateQueueWorker()
-	}
-
 	// Serve static files
 	app.Static("/", STATIC_PATH)
 
@@ -125,182 +51,6 @@ func main() {
 	app.Use(recover.New())
 
 	app.Listen(":5000")
-}
-
-type HuggingFaceRequest struct {
-	Inputs     string `json:"inputs"`
-	Parameters struct {
-		MaxNewTokens int     `json:"max_new_tokens"`
-		Temperature  float32 `json:"temperature"`
-		TopP         float32 `json:"top_p"`
-	} `json:"parameters"`
-	Options struct {
-		WaitForModel bool `json:"wait_for_model"`
-	} `json:"options"`
-}
-
-type LlamaCppRequest struct {
-	Prompt      string  `json:"prompt"`
-	NPredict    int     `json:"n_predict"`
-	Temperature float32 `json:"temperature"`
-}
-
-func postJsonReq(url string, reqBody interface{}, auth bool) *http.Response {
-	httpClient := &http.Client{}
-	req, err := http.NewRequest("POST", url, nil)
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
-	req.Header.Set("Content-Type", "application/json")
-	if auth {
-		req.Header.Set("Authorization", "Bearer "+API_KEY)
-	}
-
-	reqBodyJson, err := json.Marshal(reqBody)
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-
-	req.Body = io.NopCloser(strings.NewReader(string(reqBodyJson)))
-
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	return resp
-}
-
-func sendHfRequest(q_req GenerateRequest) *string {
-	// json body
-	reqBody := HuggingFaceRequest{
-		Inputs: q_req.Prompt,
-		Parameters: struct {
-			MaxNewTokens int     `json:"max_new_tokens"`
-			Temperature  float32 `json:"temperature"`
-			TopP         float32 `json:"top_p"`
-		}{
-			MaxNewTokens: 200,
-			Temperature:  0.0,
-			TopP:         1.0,
-		},
-		Options: struct {
-			WaitForModel bool `json:"wait_for_model"`
-		}{
-			WaitForModel: true,
-		},
-	}
-
-	resp := postJsonReq(MODEL_ENDPOINT, reqBody, true)
-	if resp == nil {
-		return nil
-	}
-	defer resp.Body.Close()
-	var respBody []map[string]string
-	// json unmarshal
-	if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
-		log.Println(err)
-		return nil
-	}
-	generated := respBody[0]["generated_text"]
-	return &generated
-}
-
-func sendLlamaCppRequest(q_req GenerateRequest) *string {
-	// json body
-	reqBody := LlamaCppRequest{
-		Prompt:      q_req.Prompt,
-		NPredict:    2048,
-		Temperature: 0.0,
-	}
-
-	resp := postJsonReq(LLAMACPP_ENDPOINT, reqBody, false)
-	if resp == nil {
-		return nil
-	}
-	defer resp.Body.Close()
-	resBody, e := io.ReadAll(resp.Body)
-	if e != nil {
-		log.Println(e)
-		return nil
-	}
-	jsonStr := string(resBody)
-	resBytes := []byte(jsonStr)
-	var respBody map[string]interface{}
-	// json unmarshal
-	if err := json.Unmarshal(resBytes, &respBody); err != nil {
-		log.Println("Error! ", err)
-		return nil
-	}
-	generated := q_req.Prompt + respBody["content"].(string)
-	return &generated
-}
-
-func generateQueueWorker() {
-	handleGenerateRequest := func(q_req GenerateRequest) *GenerateResponse {
-		log.Println("Received generate request:\n", q_req.Prompt)
-		if q_req.Prompt == "" {
-			return &GenerateResponse{Generated: ""}
-		}
-		// clip Prompt to max 2000 characters
-		if len(q_req.Prompt) > 2000 {
-			log.Println("Clipping prompt to 2000 characters")
-			q_req.Prompt = q_req.Prompt[:2000]
-			log.Println("Clipped prompt:\n", q_req.Prompt)
-		}
-		// check cache
-		cache.Lock.RLock()
-		if val, ok := cache.Requests[q_req.Prompt]; ok {
-			cache.Lock.RUnlock()
-			log.Println("Cache hit for: ", q_req.Prompt)
-			return val
-		}
-		cache.Lock.RUnlock()
-
-		var reqRes *string
-		if ENGINE == "HF" {
-			reqRes = sendHfRequest(q_req)
-		} else if ENGINE == "LLAMACPP" {
-			reqRes = sendLlamaCppRequest(q_req)
-		}
-		if reqRes == nil {
-			return nil
-		}
-		generated := *reqRes
-		log.Println("Generated:\n", generated)
-
-		res := GenerateResponse{Generated: generated}
-		// add to cache
-		cache.Lock.Lock()
-		cache.Requests[q_req.Prompt] = &res
-		cache.Lock.Unlock()
-		return &res
-	}
-
-	for {
-		select {
-		case q_req := <-generateQueue:
-			q_req.responseQueue <- handleGenerateRequest(q_req.req)
-		}
-	}
-}
-
-func generateEndpoint(c *fiber.Ctx) error {
-	var req GenerateRequest
-	if err := c.BodyParser(&req); err != nil {
-		return err
-	}
-	queue := make(chan *GenerateResponse)
-	generateQueue <- GenerateRequestQueued{req: req, responseQueue: queue}
-	resp := <-queue
-	if resp == nil {
-		return c.Status(500).JSON(&fiber.Map{
-			"error": "Internal Server Error",
-		})
-	}
-	return c.JSON(resp)
 }
 
 func createLogFile() *os.File {
